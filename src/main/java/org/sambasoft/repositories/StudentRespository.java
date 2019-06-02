075154f97409fd7e79f73c07562586940b87a754
francis <francis@Francis-PC>
2019-06-02 15:24:26 +0200

    firt commit

--- src/main/java/org/sambasoft/repositories/StudentRespository.java
+++
@@ -1,9 +0,0 @@
-package org.sambasoft.repositories;
-
-import org.sambasoft.entities.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface StudentRespository  extends JpaRepository<Student, Long>{
-
-}
